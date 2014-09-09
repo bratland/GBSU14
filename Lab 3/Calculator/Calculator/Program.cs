397eba0af232cc80d3bc2ccf72ac32ae1b1dd920
Anders Bratland <anders@bratland.se>
2014-09-09 15:38:51 +0200

    Med parenteser och buggar

--- Lab 3/Calculator/Calculator/Program.cs
+++ Lab 3/Calculator/Calculator/Program.cs
@@ -1,55 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var expr = "2+5";
-            var expr1 = "2+5*10-6/3";
-            var expr2 = "5*10";
-            var expr3 = "2+5*10+1";
+            string expr = "(2+5)*(3+7)";
+            //var expr1 = "2+5*10-6/3";
+            //var expr2 = "5*10";
+            //var expr3 = "2+5*10+1";
 
-            Console.WriteLine(expr +  " = " + EvalExpr(expr));
-            Console.WriteLine(expr1 + " = " + EvalExpr(expr1));
-            Console.WriteLine(expr2 + " = " + EvalExpr(expr2));
-            Console.WriteLine(expr3 + " = " + EvalExpr(expr3));
+            Console.WriteLine(expr + " = " + EvalExpr(expr));
+            //Console.WriteLine(expr1 + " = " + EvalExpr(expr1));
+            //Console.WriteLine(expr2 + " = " + EvalExpr(expr2));
+            //Console.WriteLine(expr3 + " = " + EvalExpr(expr3));
+            Console.ReadLine();
         }
 
         private static int EvalExpr(string expr)
         {
-            int result = 0;
+            if (expr == "") return 0;
+
+            if (expr.Contains('('))
+            {
+                var expressions = SplitInTwo(expr, '(');
+                return EvalExpr(expressions[0]) + InnerExpression(expressions[1]);
+            }
+
+
             if (expr.Contains('+'))
             {
-                var expressions = expr.Split(new[] { '+' }, 2);
-                result = EvalExpr(expressions[0]) + EvalExpr(expressions[1]);
-            }
-            else if (expr.Contains('-'))
-            {
-                var expressions = expr.Split(new []{'-'},2);
-                result = EvalExpr(expressions[0])- EvalExpr(expressions[1]);
-            }
-            else if (expr.Contains('*'))
-            {
-                var expressions = expr.Split(new[] { '*' }, 2);
-                result = EvalExpr(expressions[0]) * EvalExpr(expressions[1]);
-
-            } else if (expr.Contains('/'))
-            {
-                var expressions = expr.Split(new[] { '/' }, 2);
-                result = EvalExpr(expressions[0]) / EvalExpr(expressions[1]);
-            }
-            else
-            {
-                result = int.Parse(expr);
+                var expressions = SplitInTwo(expr, '+');
+                return EvalExpr(expressions[0]) + EvalExpr(expressions[1]);
             }
 
-            return result;
+            if (expr.Contains('-'))
+            {
+                var expressions = SplitInTwo(expr, '-');
+                return EvalExpr(expressions[0]) - EvalExpr(expressions[1]);
+            }
+
+            if (expr.Contains('*'))
+            {
+                var expressions = SplitInTwo(expr, '*');
+                return EvalExpr(expressions[0])*EvalExpr(expressions[1]);
+            }
+
+            if (expr.Contains('/'))
+            {
+                var expressions = SplitInTwo(expr, '/');
+                return EvalExpr(expressions[0])/EvalExpr(expressions[1]);
+            }
+
+
+            return int.Parse(expr);
+        }
+
+        private static int InnerExpression(string expr)
+        {
+            if (expr.Contains('('))
+            {
+                if (expr.IndexOf('(') < expr.IndexOf(')'))
+                {
+                    return EvalExpr(expr);
+                }
+            }
+
+            var temp = SplitInTwo(expr, ')');
+            return EvalExpr(temp[0]) + EvalExpr(temp[1]);
+
+        }
+
+        private static string[] SplitInTwo(string expr, char splitChar)
+        {
+            return expr.Split(new[] {splitChar}, 2);
         }
     }
 }
