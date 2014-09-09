c5f6107480179817f89758c0d0ed41818fb0de4e
Anders Bratland <anders@bratland.se>
2014-09-09 11:40:33 +0200

    Fib labb

---
+++ Lab 3/Fib1/Fib1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fib1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Fib(i));
+            }
+        }
+
+        private static int Fib(int pos)
+        {
+            if (pos <= 1)
+                return 1;
+
+            return Fib(pos - 1) + Fib(pos - 2);
+        }
+    }
+}
