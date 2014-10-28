e737f8e1885af86e637131a18ffceaf5140ce0a9
Anders Bratland <anders@bratland.se>
2014-10-28 09:15:49 +0100

    Initial TDD demo

---
+++ Lektion 9/TddDemo/StringCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
+
+namespace StringCalculatorTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        private readonly Calculator _calc;
+        public CalculatorTests()
+        {
+            _calc = new Calculator();
+        }
+
+        [TestMethod]
+        public void AddMethodExists()
+        {
+            var result = _calc.Add("5");
+        }
+
+        [TestMethod]
+        public void EmptyStringReturnsZero()
+        {
+            var result = _calc.Add("");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void StringWithOneNumberReturnsNumber()
+        {
+            var result = _calc.Add("5");
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void StringWithOneandTwoReturnsThree()
+        {
+            var result = _calc.Add("1,2");
+            Assert.AreEqual(3, result);
+        }
+
+        public void TestTemplate()
+        {
+            //Arrange
+            // var calc = new Calculator();
+            //Act
+            var result = _calc.Add("1,2");
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+
+        public void TestTemplate2()
+        {
+            //Given - A calulator should add two numbers, 1 and 2.
+            // var calc = new Calculator();
+            // int number1 = 1;
+            // int number2 = 2;
+            //When
+            var result = _calc.Add("1,2");
+            //Then
+            Assert.AreEqual(3, result);
+        }
+
+
+    }
+}
